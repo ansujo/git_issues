22340c4a81b0f9a0748135d4c8c1373807f7a6ef
MitsogoTest <mitsogotest@MitsogoTests-MacBook-Air-2.local>
2025-10-21 20:51:31 +0530

    urls is updated

--- be/issues/urls.py
+++ be/issues/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, ProjectViewSet, IssueViewSet
+from .views import UserViewSet, ProjectViewSet, IssueViewSet,PullReqViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'projects', ProjectViewSet)
 router.register(r'issues', IssueViewSet)
+router.register(r'pullreq',PullReqViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
