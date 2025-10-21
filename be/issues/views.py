fac6458754a81a0428b440d82ee68633622f7ee6
MitsogoTest <mitsogotest@MitsogoTests-MacBook-Air-2.local>
2025-10-21 19:42:10 +0530

    updated the views

--- be/issues/views.py
+++ be/issues/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from .models import User, Project, Issue
-from .serializers import UserSerializer, ProjectSerializer, IssueSerializer
+from .models import User, Project, Issue,PullReq
+from .serializers import UserSerializer, ProjectSerializer, IssueSerializer, PullReqSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -24,5 +24,20 @@ class IssueViewSet(viewsets.ModelViewSet):
         return queryset
 
     def perform_create(self, serializer):
+        project_id = self.request.data.get('project')
+        serializer.save(project_id=project_id)
+
+class PullReqViewSet(viewsets.ModelViewSet):
+    queryset =PullReq.objects.all()
+    serializer_class = PullReqSerializer
+
+    def get_queryset(self):
+        queryset=super().get_queryset()
+        project_id = self.request.query_params.get('project')
+        if project_id:
+            queryset=queryset.filter(project_id=project_id)
+        return queryset
+    
+    def perform_create(self,serializer):
         project_id = self.request.data.get('project')
         serializer.save(project_id=project_id)
