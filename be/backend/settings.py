0803516f2217bd8282c36cc9cc2df6d046d6cfb0
MitsogoTest <mitsogotest@MitsogoTests-MacBook-Air-2.local>
2025-10-29 13:48:20 +0530

    added postgres

--- be/backend/settings.py
+++ be/backend/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',  # or '127.0.0.1'
+        'PORT': '5432',       # default PostgreSQL port
     }
 }
 
